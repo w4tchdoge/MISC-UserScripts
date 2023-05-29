4e0ae2dc277a33dfae0e872eec4d0aacb0c464be
w4tchdoge <destinyftw101@gmail.com>
2023-05-29 16:23:06 +0100

    Update AO3_Main_Nav_User_Bookmarks.user.js

--- AO3_Main_Nav_User_Bookmarks.user.js
+++ AO3_Main_Nav_User_Bookmarks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name           AO3: User Bookmarks Button on main navbar
+// @name           AO3: Bookmarks Button on Main Navbar
 // @namespace      https://github.com/w4tchdoge
-// @version        1.0.0-20230524_034317
+// @version        1.0.0-20230529_162148
 // @description    Adds a button called "Bookmarked Fics" to the main navbar (where the search bar is). Inspired by elli-lili-lunch's "Put Bookmarks Button on AO3 Home", which can be found at https://greasyfork.org/en/scripts/440048
 // @author         w4tchdoge
 // @homepage       https://github.com/w4tchdoge/MISC-UserScripts
@@ -15,18 +15,18 @@
 	`use strict`;
 
 	// Get url to user bookmark page
-	var bookmarks_url = document.evaluate(`.//div[@id="greeting"]//li//a[text()[contains(.,'My Bookmarks')]]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.getAttribute(`href`);
+	var bkmrks_url = document.evaluate(`.//div[@id="greeting"]//li//a[text()[contains(.,'My Bookmarks')]]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.getAttribute(`href`);
 
 	// Create Bookmark button element
-	var bookmarks_btn = Object.assign(document.createElement(`li`), {
+	var bkmrks_btn = Object.assign(document.createElement(`li`), {
 		id: `navbar_bookmarks_button`,
-		innerHTML: `<a href='${bookmarks_url}'><span class='dropdown-toggle'>Bookmarked Fics</span></a>`
+		innerHTML: `<a href='${bkmrks_url}'><span class='dropdown-toggle'>Bookmarked Fics</span></a>`
 	});
 
 	// Find Search bar
-	var search_bar = document.querySelector(`ul.primary.navigation.actions li.search`);
+	var srch_bar = document.querySelector(`ul.primary.navigation.actions li.search`);
 
 	// Add created element before search bar
-	search_bar.before(bookmarks_btn);
+	srch_bar.before(bkmrks_btn);
 
 })();
