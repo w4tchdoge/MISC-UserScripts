b363201761a64b19def36c4c599966122578d444
w4tchdoge <w4tchdoge@protonmail.com>
2023-06-10 02:08:25 +0100

    Update By Other people/AO3- Kudoshits ratio MOD.user.js

--- By Other people/AO3- Kudoshits ratio MOD.user.js
+++ By Other people/AO3- Kudoshits ratio MOD.user.js
@@ -3,7 +3,7 @@
 // @description  Replace hitcount with kudos/hits percentage. Sort works on the page by this ratio.
 // @namespace	   https://greasyfork.org/scripts/3144-ao3-kudos-hits-ratio
 // @author	     Min
-// @version	     1.5.1-20230610_020342
+// @version	     1.5.1-20230610_020811
 // @history	     1.4 - always show hits on stats page, require jquery (for firefox)
 // @history	     1.3 - works for statistics, option to show hitcount
 // @history	     1.2 - makes use of new stats classes
@@ -9690,7 +9690,7 @@ var [ratio_red, ratio_yellow, ratio_green] = [`#A00000`, `#9F8600`, `#289200`];
 
 					// add ratio stats
 					var ratio_label = $('<dt class="kudoshits"></dt>').text('Kudos/Hits:');
-					var ratio_value = $('<dd class="kudoshits"></dd>').text(percents_print + '%').css('color', '#2a2a2a');
+					var ratio_value = $('<dd class="kudoshits"></dd>').text(percents_print + '%');
 					hits_value.after('\n', ratio_label, '\n', ratio_value);
 
 					if (colourbg) {
