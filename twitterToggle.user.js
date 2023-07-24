3ed9ce0d5745563ea157773c1d895a793feea5b2
w4tchdoge <w4tchdoge@protonmail.com>
2023-07-25 00:16:35 +0100

    twitterToggle v2.0.0 Rewrote the thing once I realised there are clickable DOM elements that take you back and forth between Old and New Twitter

--- twitterToggle.user.js
+++ twitterToggle.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Old/New Twitter Toggle
 // @namespace      https://github.com/w4tchdoge
-// @version        1.0.0
+// @version        2.0.0-20230725_001425
 // @description    Adds ability to switch back and forth between dimdem's Old Twitter layout and the current Twitter layout
 // @author         w4tchdoge
 // @homepage       https://github.com/w4tchdoge/MISC-UserScripts
@@ -12,25 +12,21 @@
 // @grant          GM.registerMenuCommand
 // @require        https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @license        AGPL-3.0-or-later
+// @history        2.0.0 — Rewrote the thing once I realised there are clickable DOM elements that take you back and forth between Old and New Twitter
+// @history        1.0.0 — Initial release
 // ==/UserScript==
 
 (function () {
-	`use strict`;
-
-	const curr_pgURL = window.location.href,
-		new_twit_str = `newtwitter=true`;
+	'use strict';
 
 	function toggleTwitter() {
-		switch (curr_pgURL.includes(new_twit_str)) {
+		switch (window.location.href.includes('newtwitter=true')) {
 			case true:
-				const regex = /(https?:.*)\?newtwitter=true/i;
-				var base_tweet_URL = curr_pgURL.replace(regex, `$1`);
-
-				window.location.href = base_tweet_URL;
+				document.evaluate('.//a[text()[contains(.,"Open this page in OldTwitter")]]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click();
 				break;
 
 			case false:
-				window.location.href = `${curr_pgURL}?${new_twit_str}`;
+				document.querySelector('#about > a').click();
 				break;
 
 			default:
@@ -42,14 +38,14 @@
 	/* Handler for the Keyboard Shortcut for Toggle Twitter */
 	function toggleTwitter_handler(e) {
 		/* Keyboard Shortcut for Toggle Twitter — Shift + Alt + K */
-		if (e.shiftKey && e.altKey && e.code === `KeyK`) {
+		if (e.shiftKey && e.altKey && e.code === 'KeyK') {
 			/* Execute  */
 			toggleTwitter();
 		}
 	}
 	/* Register the toggleTwitter handler */
-	document.addEventListener(`keyup`, toggleTwitter_handler, false);
+	document.addEventListener('keyup', toggleTwitter_handler, false);
 
-	GM.registerMenuCommand(`Toggle Twitter`, toggleTwitter);
+	GM.registerMenuCommand('Toggle Twitter', toggleTwitter);
 
 })();
