b485c1491b5a4119885eaea38b461903645e4925
w4tchdoge <destinyftw101@gmail.com>
2023-09-04 19:55:35 +0100

    WSS_Change_AO3_Link v1.0.0
    copy contents of local file onto repo.

--- WSS_Change_AO3_Link.user.js
+++ WSS_Change_AO3_Link.user.js
@@ -1,1 +1,52 @@
-init
+// ==UserScript==
+// @name           Worm Story Search: Link AO3 Stories to 1st CH
+// @namespace      https://github.com/w4tchdoge
+// @version        1.0.0
+// @description    Changes the link of the story to go to the first chapter on AO3 if story is on AO3
+// @author         w4tchdoge
+// @homepage       https://github.com/w4tchdoge/MISC-UserScripts
+// @updateURL      https://github.com/w4tchdoge/MISC-UserScripts/raw/main/WSS_Change_AO3_Link.user.js
+// @downloadURL    https://github.com/w4tchdoge/MISC-UserScripts/raw/main/WSS_Change_AO3_Link.user.js
+// @match          *://wormstorysearch.com/*
+// @match          *://www.wormstorysearch.com/*
+// @license        AGPL-3.0-or-later
+// @run-at         document-idle
+// ==/UserScript==
+
+(function () {
+	`use strict`;
+
+	// Regex for extracting work URL without specific chapter
+	const re_wu = /(^https?:\/\/)(.*\.)?(archiveofourown\.org)(.*?)(\/works\/\d+)\/?.*$/gmi;
+
+	// Get Table Rows
+	var table_rows = document.querySelector('#stories-searchable-table tbody.rows');
+
+	// Get Array of Stories
+	var stories_arr = Array.from(table_rows.children);
+
+	// Iterate on the array of Stories
+	stories_arr.forEach((storyELM) => {
+
+		// Try to get the blue box next to the title that says AO3
+		var AO3_xp = `.//td[contains(concat(" ",normalize-space(@class)," ")," title ")]//a[contains(@href,"archiveofourown.org")]`;
+		var AO3_tag = document.evaluate(AO3_xp, storyELM, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+		if (Boolean(AO3_tag)) { // Run only if said blue box exists
+			// Get the AO3 link
+			var AO3_link = AO3_tag.getAttribute('href');
+
+			// Remove all extraneous parts of the URL, leaving you with a URL in the format https://archiveofourown.org/works/{WORK_ID}
+			AO3_link = AO3_link.replace(re_wu, '$1$3$5');
+
+			// Replace all instances of old AO3 URL with the new one
+			Array.from(storyELM.querySelectorAll('td.title a[data-track]')).forEach((el) => {
+				el.href = AO3_link;
+			});
+
+			// Debug line of code, you can ignore it
+			// console.log(`${storyELM.querySelector('td.title a[data-track]').href} - ${storyELM.querySelector('td.title a[data-track]').textContent}`);
+		}
+	});
+
+})();
