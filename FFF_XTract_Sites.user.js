f5ba321309f7e48a38b927b91098da22e9c3cf55
w4tchdoge <destinyftw101@gmail.com>
2024-01-12 18:20:30 +0000

    FFFXS v1.0.1
    Add copying site list as both standard RegEx and RegEx suitable for TextMate Language JSON files

--- FFF_XTract_Sites.user.js
+++ FFF_XTract_Sites.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           FFF - Extract Supported Sites
 // @namespace      https://github.com/w4tchdoge
-// @version        1.0.0-20240112_174700
+// @version        1.0.1-20240112_181800
 // @description    Extract the list of sites supported by the FanFicFare Calibre Plugin
 // @author         w4tchdoge
 // @homepage       https://github.com/w4tchdoge/MISC-UserScripts
@@ -14,24 +14,36 @@
 // @grant          GM.setClipboard
 // @require        https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @license        AGPL-3.0-or-later
+// @history        1.0.1 — Add copying site list as both standard RegEx and RegEx suitable for TextMate Language JSON files
 // @history        1.0.0 — Initial commit
 // ==/UserScript==
 
 (function () {
 	`use strict`;
 
-	var sites_arr = Array.from(document.querySelectorAll('#wiki-body h4')).slice(0, -2);
+	var sites_arr = Array.from(document.querySelectorAll(`#wiki-body h4`)).slice(0, -2);
 
 	sites_arr.forEach(function (elm, i, arr) {
 		arr[i] = arr[i].textContent;
 	});
 
 	function RawSiteList() {
-		var paste = sites_arr.join('\n')
-		GM.setClipboard(paste)
+		var paste = sites_arr.join(`\n`);
+		GM.setClipboard(paste);
 	}
 
-	GM.registerMenuCommand('Copy Site List as RegEx')
-	GM.registerMenuCommand('Copy Raw Site List', RawSiteList)
+	function RegExSiteList() {
+		var known_sections_regex = `^\\[(${sites_arr.join(`|`)})\\]\\s*`;
+		GM.setClipboard(known_sections_regex);
+	}
+
+	function JSON_escd_RegExSiteList() {
+		var json_escd_known_sections_regex = `^\\[(${sites_arr.join(`|`)})\\]\\s*`.replaceAll(/(?<!\\)\\(?!\\)/gi, `\\\\`);
+		GM.setClipboard(json_escd_known_sections_regex);
+	}
+
+	GM.registerMenuCommand(`Copy Site List as JSON Escaped RegEx`, JSON_escd_RegExSiteList);
+	GM.registerMenuCommand(`Copy Site List as RegEx`, RegExSiteList);
+	GM.registerMenuCommand(`Copy Raw Site List`, RawSiteList);
 
 })();
