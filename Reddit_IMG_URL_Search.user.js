7b7ba6c6ae5356232ed4fb674d74a70100c0184b
w4tchdoge <w4tchdoge@protonmail.com>
2024-02-20 15:03:47 +0500

    RIUS v2.1.0
    Add capability to work on redd.it URLs where the filename is {Post Title}-v0-{IMG URL}

--- Reddit_IMG_URL_Search.user.js
+++ Reddit_IMG_URL_Search.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Reddit IMG URL Search
 // @namespace      https://github.com/w4tchdoge
-// @version        2.0.2-20231228_182342
+// @version        2.1.0-20240220_150020
 // @description    Searches Reddit for IMG URLs based on the filename of the current image. Initial support is for Discord attachments
 // @author         w4tchdoge
 // @homepage       https://github.com/w4tchdoge/MISC-UserScripts
@@ -16,6 +16,7 @@
 // @require        https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @run-at         document-start
 // @license        AGPL-3.0-or-later
+// @history        2.1.0 — Add capability to work on redd.it URLs where the filename is {Post Title}-v0-{IMG URL}
 // @history        2.0.2 — Change script metadata to indicate that it should run as soon as possible, as the script only requires the current page URL to function
 // @history        2.0.1 — Remove unnecessary bits of code introduced in v2.0.0
 // @history        2.0.0 — Adds capability to work on i.redd.it and preview.redd.it URLs
@@ -29,31 +30,44 @@
 	// Gets the URL of the current webpage
 	const page_URL = new URL(window.location.href);
 
-	// Function for getting the filename without file extension when the image is on the redd.it domain
-	function reddit_domain_Reddit_IMG_URL_srch() {
-
-		// Construct filename without extension
-		// As this is a redd.it url it is simpler due to being able to the pathname of the URL containing only the filename
-		var filename_noExt = page_URL.pathname.split(`.`)[0].slice(1);
-
-		return filename_noExt;
-	}
-
 	// Function for getting the filename without file extension when the image isn't on the redd.it domain
 	function Generic_Reddit_IMG_URL_srch() {
 
 		// Extracts the filename without the file extensions from the URL
 		// The first .split function creates an array from the pathname using "/" as a delimiter
-		// The .slice function removes all but the last element from the array created by the first .split, which is the raw filename
+		// The first .slice function removes all but the last element from the array created by the first .split, which is the raw filename
 		// the .toString function converts the array of 1 element into a string
 		// The second .split function splits the filename into separate chunks based on 3 separate delimiters
-		var filename_parts = page_URL.pathname.split(`/`).slice(-1).toString().split(/[\(\)._-]/);
-		// The .find function searches the filename_parts array for an element with length of 13 which is the length of the filenames of image files uploaded to Reddit
-		var filename_noExt = filename_parts.find((elem) => {
+		// The second .slice function removes the element of the array which includes the file extension
+		var filename_parts = page_URL.pathname.split(`/`).slice(-1).toString().split(/[\(\)._-]/).slice(0, -1);
+		// The .findLast function searches the filename_parts array for the last element with length of 13 which is the length of the filenames of image files uploaded to Reddit
+		var filename_noExt = filename_parts.findLast((elem) => {
 			return elem.length > 12;
 		});
 
 		return filename_noExt;
+	}
+
+	// Function for getting the filename without file extension when the image is on the redd.it domain
+	function reddit_domain_Reddit_IMG_URL_srch() {
+
+		// Check if reddit filename is longer than 13 characters
+		// If it is, use method used on non redd.it domains
+		var filename_check = page_URL.pathname.split(`.`)[0].slice(1);
+		if (filename_check.length > 13) {
+			var filename_parts = page_URL.pathname.split(`/`).slice(-1).toString().split(/[\(\)._-]/).slice(0, -1);
+			var filename_noExt = filename_parts.findLast((elem) => {
+				return elem.length > 12;
+			});
+			return filename_noExt;
+
+		} else {
+			// Construct filename without extension
+			// As this is a redd.it url it is simpler due to being able to the pathname of the URL containing only the filename
+			var filename_noExt = page_URL.pathname.split(`.`)[0].slice(1);
+
+			return filename_noExt;
+		}
 	}
 
 	function Reddit_IMG_URL_srch() {
