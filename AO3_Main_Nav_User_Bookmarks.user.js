9c05fe31fa80280665226e11b7d36f618e6f0b42
w4tchdoge <destinyftw101@gmail.com>
2024-04-04 15:53:09 +0500

    AO3: Bookmarks Button on Main Navbar v1.0.4
    Rollback 1.0.2 because I've realised that having this script running sidewide was the intention and I was a big dumb by removing that

--- AO3_Main_Nav_User_Bookmarks.user.js
+++ AO3_Main_Nav_User_Bookmarks.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name           AO3: Bookmarks Button on Main Navbar
 // @namespace      https://github.com/w4tchdoge
-// @version        1.0.3-20240404_154325
+// @version        1.0.4
 // @description    Adds a button called "Bookmarked Fics" to the main navbar (where the search bar is). Inspired by elli-lili-lunch's "Put Bookmarks Button on AO3 Home", which can be found at https://greasyfork.org/en/scripts/440048
 // @author         w4tchdoge
 // @homepage       https://github.com/w4tchdoge/MISC-UserScripts
-// @match          *://archiveofourown.org/*works/*
-// @exclude        *://archiveofourown.org/*works/*/bookmarks
+// @match          *://archiveofourown.org/*
 // @icon           https://archiveofourown.org/favicon.ico
 // @run-at         document-idle
 // @license        AGPL-3.0-or-later
-// @history        1.0.3 — Modify XPath to get the existing "My Bookmarks" button to accommodate the DOM element with id "greeting" not being a div element
+// @history        1.0.4 — Rollback 1.0.2 because I've realised that having this script running sidewide was the intention and I was a big dumb by removing that
+// @history        1.0.3 — Modify the XPath to get the existing "My Bookmarks" button to accommodate the DOM element with id "greeting" not being a div element
 // @history        1.0.2 — Modify the match rule so that it matches collections/*/works URLs as well; Add an exlude role so it doesn't work on works/*/bookmarks pages as it isn't designed to
 // @history        1.0.1 — Change the displayed text to be the same as in the original bookmarks button from the user dropdown area
 // @history        1.0.0 — Initial Release
