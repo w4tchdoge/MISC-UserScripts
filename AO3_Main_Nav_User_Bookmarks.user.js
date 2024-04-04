edc2f5e0ee120ca2cf130d4fb875d8f1a6c161b2
w4tchdoge <destinyftw101@gmail.com>
2024-04-04 15:46:31 +0500

    AO3: Bookmarks Button on Main Navbar v1.0.3
    Modify the XPath to get the existing "My Bookmarks" button to accommodate the DOM element with id "greeting" not being a div element

--- AO3_Main_Nav_User_Bookmarks.user.js
+++ AO3_Main_Nav_User_Bookmarks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           AO3: Bookmarks Button on Main Navbar
 // @namespace      https://github.com/w4tchdoge
-// @version        1.0.2-20240314_210220
+// @version        1.0.3-20240404_154325
 // @description    Adds a button called "Bookmarked Fics" to the main navbar (where the search bar is). Inspired by elli-lili-lunch's "Put Bookmarks Button on AO3 Home", which can be found at https://greasyfork.org/en/scripts/440048
 // @author         w4tchdoge
 // @homepage       https://github.com/w4tchdoge/MISC-UserScripts
@@ -10,6 +10,7 @@
 // @icon           https://archiveofourown.org/favicon.ico
 // @run-at         document-idle
 // @license        AGPL-3.0-or-later
+// @history        1.0.3 — Modify XPath to get the existing "My Bookmarks" button to accommodate the DOM element with id "greeting" not being a div element
 // @history        1.0.2 — Modify the match rule so that it matches collections/*/works URLs as well; Add an exlude role so it doesn't work on works/*/bookmarks pages as it isn't designed to
 // @history        1.0.1 — Change the displayed text to be the same as in the original bookmarks button from the user dropdown area
 // @history        1.0.0 — Initial Release
@@ -19,7 +20,7 @@
 	`use strict`;
 
 	// Get the user bookmark page button from the user dropdown
-	var bkmrks_btn = document.evaluate(`.//div[@id="greeting"]//li//a[text()[contains(.,'My Bookmarks')]]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+	var bkmrks_btn = document.evaluate(`.//*[@id="greeting"]//li//a[text()[contains(.,'My Bookmarks')]]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 
 	// Get the URL and displayed text from the user bookmarks button
 	var bkmrks_url = bkmrks_btn.getAttribute(`href`),
