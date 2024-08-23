f2ac51595adbc86ae2d8b89192f2af4624d90c6e
w4tchdoge <destinyftw101@gmail.com>
2024-08-23 17:18:14 +0500

    WSS Link To Thread v1.0.0
    initial commit

---
+++ WSS_Link_To_Thread.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name           Worm Story Search: Change Threadmark page link to Thread page link
+// @namespace      https://github.com/w4tchdoge
+// @version        1.0.0-20240823_171716
+// @description    Remove the `/threadmarks` at the end of all SV/SB/QQ URLs to have them go to the thread page instead of the threadmarks page
+// @author         w4tchdoge
+// @homepage       https://github.com/w4tchdoge/MISC-UserScripts
+// @updateURL      https://github.com/w4tchdoge/MISC-UserScripts/raw/main/WSS_Link_To_Thread.user.js
+// @downloadURL    https://github.com/w4tchdoge/MISC-UserScripts/raw/main/WSS_Link_To_Thread.user.js
+// @match          *://wormstorysearch.com/*
+// @match          *://www.wormstorysearch.com/*
+// @icon           http://wormstorysearch.com/favicon.png
+// @run-at         document-idle
+// @license        AGPL-3.0-or-later
+// @history        1.0.0 — Initial commit
+// ==/UserScript==
+
+(function () {
+	`use strict`;
+
+	// Get Table Rows
+	const table_rows = document.querySelector('#stories-searchable-table tbody.rows');
+
+	// Get Array of Stories
+	const stories_arr = Array.from(table_rows.children);
+
+	// Iterate on the array of Stories
+	stories_arr.forEach((storyELM) => {
+
+		Array.from(storyELM.querySelectorAll('td.title a[data-track][href*="/threads/"]')).forEach((elm, index, arr) => {
+			const re_wu = /(https?:\/\/forums?\..*?\.com\/threads\/).*\.(\d+\/)/gmi;    /* Regex for extracting work URL without thread name */
+			const re_https = /http(?!s)/i;
+			const re_pgnum = /(page-\d+)|(reader.)|(threadmarks.?)/i;
+
+			const initial_url = new URL(elm.getAttribute(`href`));
+			const output_url = `${initial_url.origin.replace(re_https, `https`)}${initial_url.pathname.split(re_pgnum).at(0)}`.replace(re_wu, `$1$2`).slice(0, -1);
+
+			// console.log(`#${index + 1}. ${output_url}`);
+
+			elm.setAttribute(`href`, output_url);
+		});
+
+	});
+})();
