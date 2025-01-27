eb0772744d92b334ec169143c63a8e0f7c291d1e
w4tchdoge <destinyftw101@gmail.com>
2025-01-27 22:05:38 +0800

    RDT DL IMG w info v0.0.1
    rename file and add userjs metadata

---
+++ Old_RDT_DL_IMG_w_info.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name           Old Reddit: Download IMG post with title
+// @namespace      https://github.com/w4tchdoge
+// @version        1.0.0
+// @description    Add a button next to the IMG domain to download the IMG with the post title, old reddit shortlink ID, and IMG name in the output filename
+// @author         w4tchdoge
+// @homepage       https://github.com/w4tchdoge/MISC-UserScripts
+// @updateURL      
+// @downloadURL    
+// @match          *://www.reddit.com/r/*/comments/*
+// @match          *://old.reddit.com/r/*/comments/*
+// @license        AGPL-3.0-or-later
+// ==/UserScript==
+
+(function () {
+	`use strict`;
+
+	
+})();
