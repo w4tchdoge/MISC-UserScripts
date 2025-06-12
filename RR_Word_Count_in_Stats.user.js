644a661625ee108a41060075be2a2f0148207e3a
w4tchdoge <w4tchdoge@protonmail.com>
2025-06-12 12:22:10 +0800

    RRWCiS v1.1.0
    Add word count next to chapter count in the ToC header bar thing

--- RR_Word_Count_in_Stats.user.js
+++ RR_Word_Count_in_Stats.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Royal Road: Add Word Count to Statistics Section
 // @namespace      https://github.com/w4tchdoge
-// @version        1.0.0-20240121_224010
+// @version        1.1.0-20250611_121836
 // @description    Adds the word count of a fiction (taken from the information tooltip in the Pages statistic) as it's own statistic
 // @author         w4tchdoge
 // @homepage       https://github.com/w4tchdoge/MISC-UserScripts
@@ -10,51 +10,90 @@
 // @match          *://*.royalroad.com/fiction/*
 // @exclude        *://*.royalroad.com/fiction/*/*/chapter/*
 // @license        AGPL-3.0-or-later
+// @run-at         document-start
+// @history        1.1.0 — Add word count next to chapter count in the ToC header bar thing
 // @history        1.0.0 — Initial release
 // ==/UserScript==
 
-(function () {
+(async function () {
 	`use strict`;
 
-	// Check if the Statistics section exists
-	var stats_page_exists = Boolean(document.querySelector(`.fiction-stats .stats-content .list-unstyled li:has(> i)`));
+	// modified from https://stackoverflow.com/a/61511955/11750206
+	function waitForElm(selector, search_root = document) {
+		return new Promise(resolve => {
+			if (search_root.querySelector(selector)) {
+				return resolve(search_root.querySelector(selector));
+			}
 
-	// Define variables in advance (this is for easier debugging on my part when I copy paste these into the console during initial testing to see if everything works)
-	var
-		statsPage_pages_elem,
-		word_count_str,
-		elm_wordcount_title,
-		elm_wordcount_data;
+			const observer = new MutationObserver(mutations => {
+				if (search_root.querySelector(selector)) {
+					observer.disconnect();
+					resolve(search_root.querySelector(selector));
+				}
+			});
 
-	// Only execute the main body of code if stats_page_exists is true
-	if (stats_page_exists == true) {
+			// If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+			observer.observe(document.documentElement, {
+				childList: true,
+				subtree: true
+			});
+		});
+	}
 
-		// Get the element which contains the word "Pages" and which also contains the word count in an informational tooltip
-		statsPage_pages_elem = document.querySelector(`.fiction-stats .stats-content .list-unstyled li:has(> i)`);
+	// wait for fic info section to load in
+	const ficinfo = await waitForElm(`div.fiction-info`);
 
-		// Get Word Count
-		word_count_str = statsPage_pages_elem.querySelector(`i`).getAttribute(`data-content`).replace(/.*calculated from (.*) words./gmi, `$1`);
+	// Get the element which contains the word "Pages" and which also contains the word count in an informational tooltip
+	const statsPage_pages_elem = ficinfo.querySelector(`.fiction-stats .stats-content .list-unstyled li:has(> i)`);
 
-		// Make the "Heading" element that describes the data below it. i.e. it says "Words"
-		elm_wordcount_title = Object.assign(document.createElement(`li`), {
+	// Get Word Count
+	const word_count_str = statsPage_pages_elem.querySelector(`i`).getAttribute(`data-content`).replace(/.*calculated from (.*) words./gmi, `$1`);
+	console.log(`
+RR WORD COUNT SCRIPT
+WORD COUNT: ${word_count_str}`);
+
+	// Make the "Heading" element that describes the data below it. i.e. it says "Words"
+	const elm_wordcount_title = (() => {
+		let outelm = Object.assign(document.createElement(`li`), {
 			id: `userscript-words-title`,
 			innerHTML: `Words :`
 		});
 
-		// Add the required classes to the above DOM element
-		elm_wordcount_title.classList.add(`bold`, `uppercase`);
+		// Add the bold and uppercase classes to make it look like the other stats page entries
+		outelm.classList.add(`bold`, `uppercase`);
 
-		// Make the "Data" element that has the actual data. i.e. it says the word count
-		elm_wordcount_data = Object.assign(document.createElement(`li`), {
+		return outelm;
+	})();
+
+	// Make the "Data" element that has the actual data. i.e. it says the word count
+	const elm_wordcount_data = (() => {
+		let outelm = Object.assign(document.createElement(`li`), {
 			id: `userscript-words-data`,
 			innerHTML: `${word_count_str}`
 		});
 
-		// Add the required classes to the above DOM element
-		elm_wordcount_data.classList.add(`bold`, `uppercase`, `font-red-sunglo`);
+		// Add the bold and uppercase classes to make it look like the other stats page entries
+		outelm.classList.add(`bold`, `uppercase`, `font-red-sunglo`);
 
-		// Add the word count elements before the page count elements
-		statsPage_pages_elem.before(elm_wordcount_title, elm_wordcount_data);
+		return outelm;
+	})();
 
-	}
+	// Add the word count elements before the page count elements
+	statsPage_pages_elem.before(elm_wordcount_title, elm_wordcount_data);
+
+	const tocbar_ch_cnt = (() => {
+		const tocbar = ficinfo.querySelector(`div.portlet > .portlet-title:has(> .caption > a#toc)`);
+		const xpath = `.//*[contains(concat(" ",normalize-space(@class)," ")," actions ")]/span[contains(normalize-space(),"Chapter")]`;
+		const chcntpar = document.evaluate(xpath, tocbar, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+		return chcntpar;
+	})();
+
+	const tocbar_wc = (() => {
+		let base_elm = tocbar_ch_cnt.cloneNode(true);
+		base_elm.textContent = `${word_count_str} Words`;
+		base_elm.style.marginRight = `0.25em`;
+		return base_elm;
+	})();
+
+	tocbar_ch_cnt.after(tocbar_wc);
 })();
