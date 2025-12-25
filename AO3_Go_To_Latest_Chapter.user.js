728707df8ee5436f328a32e5e239518943ff3c33
w4tchdoge <w4tchdoge@protonmail.com>
2025-12-26 01:54:38 +0800

    AO3 Latest Ch v2.0.0
    Add features to take you to the nav page or a specific chapter of the work
    using `/nav` and `/nav/{number}` respectively

--- AO3_Go_To_Latest_Chapter.user.js
+++ AO3_Go_To_Latest_Chapter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           AO3: Go To Latest Chapter
 // @namespace      https://github.com/w4tchdoge
-// @version        1.1.1-20251221_094646
+// @version        2.0.0-20251226_015415
 // @description    Adds a link to the chapter navigation bar to go to the latest chapter of a work. Alternative method is to add `/latest` to the end of an AO3 work URL. e.g. https://archiveofourown.org/works/{AO3_WORK_ID}/chapters/{AO3_CHAPTER_ID}/latest
 // @author         w4tchdoge
 // @homepage       https://github.com/w4tchdoge/MISC-UserScripts
@@ -9,10 +9,13 @@
 // @downloadURL    https://github.com/w4tchdoge/MISC-UserScripts/raw/main/AO3_Go_To_Latest_Chapter.user.js
 // @match          *://archiveofourown.org/*works/*
 // @match          *://archiveofourown.org/*works/*/latest
+// @match          *://archiveofourown.org/*works/*/nav
+// @match          *://archiveofourown.org/*works/*/nav/*
 // @exclude        *://archiveofourown.org/*works/*/bookmarks
 // @exclude        *://archiveofourown.org/*works/*/navigate
 // @license        AGPL-3.0-or-later
 // @run-at         document-start
+// @history        2.0.0 — Add features to take you to the nav page or a specific chapter of the work using `/nav` and `/nav/{number}` respectively
 // @history        1.1.1 — Make the "Latest ↠" button automatically take you to the start of the chapter text instead of the start of the webpage, making it consistent with the behaviour of the next & previous chapter buttons
 // @history        1.1.0 — Use the chapter index dropdown to get the latest chapter ID when already viewing a multi chapter work instead of making an fetch request to the work's /navigate page
 // @history        1.0.0 — Move the `latest_url` stuff to a function. Comment the code to the point where hopefully someone that doesn't know JS can understand what it's doing. Clean up old commented code that's no longer used. Add a description
@@ -46,9 +49,8 @@
 
 	// Gets URL of the latest chapter of a work
 	// Input is the work ID as a string
-	// Output is the latest URL as a URL object
-	async function getLatestURL(ao3_work_id) {
-
+	// Output is an array of URL objects of the the chapter URLs of the work
+	async function getChapterURLsArr(ao3_work_id, base_url = `https://archiveofourown.org`) {
 		// Define the URL to be fetched
 		const fetch_url = `https://archiveofourown.org/works/${ao3_work_id}/navigate`;
 		// Fetch the URL as UTF-8 HTML and wait for the response from the server
@@ -61,19 +63,17 @@
 		// Parse the response text as HTML and saves the output
 		const html = html_parser.parseFromString(resp_text, `text/html`);
 
-		// Search the parsed HTML of the navigate page for all links that go to a chapter
-		const chapter_links_arr = Array.from(html.querySelectorAll(`#main .chapter.index.group > li > a[href^="/works"]`));
-		// Get the href attribute of the last element in above array (which would be the latest chapter), and add the '#workskin' anchor that AO3 has for the buttons that take you to the next/prev chapter
-		const latest_ch_href = `${chapter_links_arr.at(-1).getAttribute(`href`)}#workskin`;
-		// Create a URL object from the latest chapter href
-		const latest_url = new URL(latest_ch_href, `https://archiveofourown.org`);
+		// Search the parsed HTML of the navigate page for all links that go to a chapter, and create an "ordered" array containing all of the aforementioned links as URL objects
+		const chapter_urls_arr = Array.from(html.querySelectorAll(`#main .chapter.index.group > li > a[href^="/works"]`))
+			.map(e => new URL(`${e.getAttribute(`href`)}#workskin`, base_url));
 
-		return latest_url;
+		return chapter_urls_arr;
 	}
 
 
 	// Get current page url as a URL object
 	const curr_url = new URL(window.location);
+	const curr_url_domain = curr_url.hostname;
 
 	// Get AO3 work ID
 	const work_id = (() => {
@@ -90,14 +90,24 @@
 
 	})();
 
+	const url_latest = curr_url.pathname.split(`/`).at(-1).toString().toLowerCase() == `latest`;
+	const url_nav_end = curr_url.pathname.split(`/`).at(-1).toString().toLowerCase() == `nav`;
+	const url_nav = (() => {
+		const segments = curr_url.pathname.split(`/`);
+		const nav_seg = segments.at(-2).toString().toLowerCase() == `nav`;
+		const nav_num = Number.isInteger(parseInt(segments.at(-1)));
+		if (nav_seg && nav_num) { return true; } else { return false; }
+	})();
+
+
 	// Check if URL ends in latest
-	if (curr_url.pathname.split(`/`).at(-1).toString().toLowerCase() == `latest`) { // If URL ends in latest
+	if (url_latest) { // If URL ends in latest
 
 		// Stop further loading of webpage
 		window.stop();
 
 		// Get URL for the latest chapter
-		const latest_url = await getLatestURL(work_id);
+		const latest_url = (await getChapterURLsArr(work_id, `https://${curr_url_domain}`)).at(-1);
 
 		// console.log(`latest`, curr_url, work_id, latest_url);
 
@@ -105,7 +115,35 @@
 		window.location.replace(latest_url);
 
 	}
-	else { // If URL doesn't end in latest
+	if (url_nav_end) {
+
+		window.stop();
+		window.location.replace(`https://${curr_url_domain}/works/${work_id}/navigate`);
+
+	}
+	if (url_nav) {
+
+		window.stop();
+
+		const work_chapter_urls = await getChapterURLsArr(work_id, `https://${curr_url_domain}`);
+
+		const nav_idx = (() => {
+			const usr_nav_num = parseInt(curr_url.pathname.split(`/`).at(-1));
+			if (usr_nav_num < 1) {
+				throw new Error("Please pass a value greater than 0 for the chapter you are attempting to navigate to");
+			}
+			if (usr_nav_num > work_chapter_urls.length) {
+				throw new Error("Please pass a value less than or equal to the number of published chapters for the chapter you are attempting to navigate to");
+			}
+			const idx = usr_nav_num - 1;
+			return idx;
+		})();
+		const nav_url = work_chapter_urls.at(nav_idx);
+
+		window.location.replace(nav_url);
+
+	}
+	if (!url_latest && !url_nav_end && !url_nav) {
 
 		// Get the current page's hostname
 		const curr_pg_hostname = (new URL(window.location)).hostname;
